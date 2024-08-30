142d7b100b2f2c675f53f166273496fce55f88ce
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2024-08-30 07:30:08 -0500

    Initial Setup
    Initial Setup

---
+++ index.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+import discord
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready();
+    print("I'm ready for action, or breaking, whichever comes first.")
+
+bot.run(TOKEN)
